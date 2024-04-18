2964c35d0272b83d421f1fd3aa4cbbda5b7790b7
RachaelKivuti <kivutingatha@gmail.com>
2024-04-18 13:42:42 +0300

    make a bid

--- my_counter/lib/main.dart
+++ my_counter/lib/main.dart
@@ -17,27 +17,12 @@ class MyApp extends StatelessWidget {
           false, //removing the debug banner in application
       title: 'My counter',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(
-            seedColor: const Color.fromARGB(255, 77, 6, 73)),
+            seedColor: const Color.fromARGB(255, 2, 121, 28)),
         canvasColor: Theme.of(context).colorScheme.background,
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Hello world'),
+      home: const MyHomePage(title: 'MAKE A BID'),
     );
   }
 }
@@ -45,53 +30,27 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+//the state class of the stateful widget class
 class _MyHomePageState extends State<MyHomePage> {
-  
-  int _counter = 0;
+  int _counter = 0; // value the bid starts
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _counter += 500; // each click of button increases bid by 500 ksh
     });
-    
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
@@ -114,7 +73,20 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'BID BEGINS AT 0.\n'
+              'EVERY PRESS INCREASE BID WITH 500 KSH.\n'
+              'THE BID IS NOW AT KSH:',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontFamily: 'Roboto', // Change to the font family
+                fontSize: 18, // Changing the font size
+                color: Color.fromARGB(255, 9, 110, 16), // changing font color
+                fontWeight: FontWeight.bold, // Chaning the font weight
+                fontStyle: FontStyle.italic, // Changing the font style
+                letterSpacing: 1.0, //  adjusting letter spacing
+                wordSpacing: 2.0, // adjusting word spacing
+                height: 1.5, //  adjusting line height
+              ),
             ),
             Text(
               '$_counter',
