04c93c1ddbef36d085a440e6e85b549cf495b5f7
RachaelKivuti <kivutingatha@gmail.com>
2024-04-25 21:00:18 +0300

    changes

--- staggered_animation/lib/widgets/animated_balloon.dart
+++ staggered_animation/lib/widgets/animated_balloon.dart
@@ -7,7 +7,8 @@ class AnimatedBalloonWidget extends StatefulWidget {
   State<AnimatedBalloonWidget> createState() => _AnimatedBalloonWidgetState();
 }
 
-class _AnimatedBalloonWidgetState extends State<AnimatedBalloonWidget> {
+class _AnimatedBalloonWidgetState extends State<AnimatedBalloonWidget>
+    with SingleTickerProviderStateMixin {
   late AnimationController _controller;
   late Animation<double> _animationFloatUp;
   late Animation<double> _animationGrowSize;
@@ -15,7 +16,8 @@ class _AnimatedBalloonWidgetState extends State<AnimatedBalloonWidget> {
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(duration: const Duration(seconds: 4), vsync: this);
+    _controller =
+        AnimationController(duration: const Duration(seconds: 4), vsync: this);
   }
 
   @override
@@ -26,49 +28,47 @@ class _AnimatedBalloonWidgetState extends State<AnimatedBalloonWidget> {
 
   @override
   Widget build(BuildContext context) {
-
     double _balloonHeight = MediaQuery.of(context).size.height / 2;
     double _balloonWidth = MediaQuery.of(context).size.height / 3;
-    double _balloonBottomLocation = MediaQuery.of(context).size.height -
-    _balloonHeight;
+    double balloonBottomLocation =
+        MediaQuery.of(context).size.height - _balloonHeight;
 
-  void _animationFloatUp = Tween(begin: _balloonBottomLocation, end: 0.0).animate(
-    CurvedAnimation(
-      parent: _controller,
-      curve: Interval(0.0, 1.0, curve: Curves.fastOutSlowIn),
-    ),
-  );
+    _animationFloatUp = Tween(begin: balloonBottomLocation, end: 0.0).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: const Interval(0.0, 1.0, curve: Curves.fastOutSlowIn),
+      ),
+    );
 
-  void _animationGrowSize = Tween(begin: 50.0, end: _balloonWidth).animate(
-    CurvedAnimation(
-      parent: _controller,
-      curve: Interval(0.0, 0.5, curve: Curves.elasticInOut),
-    ),
-  );
+    _animationGrowSize = Tween(begin: 50.0, end: _balloonWidth).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: const Interval(0.0, 0.5, curve: Curves.elasticInOut),
+      ),
+    );
 
-  return AnimatedBuilder(
-    animation: _animationFloatUp,
-    builder: (context, child) {
-      return Container(
-        child: child,
-        margin: EdgeInsets.only(
-          top: _animationFloatUp.value,
-        ),
-        width: _animationGrowSize.value,
-      );
-    },
-    child: GestureDetector(
-      onTap: () {
-        if (_controller.isCompleted) {
-          _controller.reverse();
-        } else {
-          _controller.forward();
-        }
+    return AnimatedBuilder(
+      animation: _animationFloatUp,
+      builder: (context, child) {
+        return Container(
+          margin: EdgeInsets.only(
+            top: _animationFloatUp.value,
+          ),
+          width: _animationGrowSize.value,
+          child: child,
+        );
       },
-      child: Image.asset('balloon.png',
-        height: _balloonHeight, width: _balloonWidth),
-    ),
+      child: GestureDetector(
+        onTap: () {
+          if (_controller.isCompleted) {
+            _controller.reverse();
+          } else {
+            _controller.forward();
+          }
+        },
+        child: Image.asset('assets/images/balloon.png',
+            height: _balloonHeight, width: _balloonWidth),
+      ),
     );
   }
 }
-  
